324141efd0116e9c55dcbd308a805ebc0fae22e5
KAMUZUKI <1437487442@qq.com>
2021-03-23 09:56:58 +0800

    html

---
+++ 实习课程/index.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+// 当网页向下滑动 30px 出现"返回顶部" 按钮
+window.onscroll = function() {
+    scrollFunction()
+};
+		 
+function scrollFunction() {
+    console.log(121);
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnTop").style.display = "block";
+    } else {
+        document.getElementById("btnTop").style.display = "none";
+    }
+}
+		 
+// 点击按钮，返回顶部
+function returnTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
